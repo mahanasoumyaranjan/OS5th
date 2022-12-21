5a8f863981551fb9b8d9674382c059f4ef19222a
mahanasoumyaranjan <116441447+mahanasoumyaranjan@users.noreply.github.com>
2022-12-21 19:58:29 +0530

    Add files via upload

---
+++ SJF (2).c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+void main()
+{
+	int i, j, k, at[100], bt[100], tat[100], wt[100], n;
+	printf("Enter the no of processes\n");
+	scanf("%d",&n);
+	printf("All the processes are arrived at 0");
+	printf("Enter burst time for the following processes\n");
+	for(i=0;i<n;i++)
+    {
+		printf("P%d",i);
+		scanf("%d",&bt[i]);
+		printf("\n");
+	}
+	sort(bt, n);
+	tat[-1] = 0;
+	wt[0] = 0;
+	for(i=0;i<n;i++)
+	{
+		tat[i] = tat[i-1]+bt[i];
+		wt[i] = tat[i]-bt[i];
+	}
+	printf("Processes\t Arrival Time\t Burst Time\t Turnaround Time\t Waiting Time\t\n");
+	for(i=0;i<n;i++)
+	{
+		printf("P%d\t\t",i);
+		printf("%d\t\t",at[i]);
+		printf("%d\t\t",bt[i]);
+		printf("%d\t\t",tat[i]);
+		printf("%d\t\t",wt[i]);
+		printf("\n");
+	}
+	float t = 0.0, w = 0.0;
+	for(i=0;i<n;i++)
+	{
+		t = t+tat[i];
+		w = w+wt[i];
+	}
+	float atat = t/n;
+	float awt = w/n;
+	printf("Average TAT is %f\n",atat);
+	printf("Average WT is %f\n",awt);
+}
+void sort(int a[], int n)
+{
+    int i, j, temp = 0;
+    for(i=1;i<n;i++)
+    {
+        for(j=1;j<n;j++)
+        {
+            if(a[j-1]>a[i])
+            {
+                temp = a[i];
+                a[i] = a[j-1];
+                a[j-1] = temp;
+            }
+        }
+    }
+}
